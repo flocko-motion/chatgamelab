56b787fa41255aec6f36f2afc3cbe4af2eace5dd
flocko-motion <florian.metzger@gmail.com>
2023-12-28 20:46:05 +0100

    feat: configure CORS with .env

--- server/router/cors.go
+++ server/router/cors.go
@@ -1,9 +1,17 @@
 package router
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
+
+var corsAllowedOrigin string
 
 func SetCorsHeaders(w http.ResponseWriter) {
+	if corsAllowedOrigin == "" {
+		corsAllowedOrigin = os.Getenv("CORS_ALLOWED_ORIGIN")
+	}
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", corsAllowedOrigin)
 	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 }
