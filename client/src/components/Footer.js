4a7bb2d90ec88f9b300e99669da29ef939ab9ac9
flocko-motion <florian.metzger@gmail.com>
2024-04-21 09:38:29 +0200

    feat: add version

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,10 +3,9 @@ import {version} from "../version.js";
 
 const Footer = () => (
     <footer className="bg-light p-1 text-center">
-         Version {version} |
             Login by <a href="https://auth0.com">Auth0</a> |
             Programmed by <a href="https://omnitopos.net">omnitopos.net</a> |
-        Produced by <a href="https://tausend-medien.de">tausend-medien.de</a>
+        Produced by <a href="https://tausend-medien.de">tausend-medien.de</a> | v{version}
     </footer>
 );
 
