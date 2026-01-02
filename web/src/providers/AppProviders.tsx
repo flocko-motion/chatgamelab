5e042f95c3e0d6349d9a7d9d5dbc5a6eed31ef45
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-02 21:32:55 +0100

    feat: integrate error boundary into app providers

--- web/src/providers/AppProviders.tsx
+++ web/src/providers/AppProviders.tsx
@@ -4,6 +4,7 @@ import { Notifications } from '@mantine/notifications';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
+import { ErrorBoundary } from '../common/components/ErrorBoundary';
 
 import { mantineTheme } from '../config/mantineTheme';
 import { queryClient } from '../config/queryClient';
@@ -11,14 +12,16 @@ import { router } from '../config/router';
 
 export function AppProviders() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider theme={mantineTheme}>
-        <ModalsProvider>
-          <Notifications />
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ModalsProvider>
-      </MantineProvider>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider theme={mantineTheme}>
+          <ModalsProvider>
+            <Notifications />
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ModalsProvider>
+        </MantineProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
