cf4417da9a4afb3663fb76b37eb2ef14c271480f
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-02 21:23:54 +0100

    feat: configure mantine notifications provider

--- web/src/providers/AppProviders.tsx
+++ web/src/providers/AppProviders.tsx
@@ -1,5 +1,6 @@
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from '@tanstack/react-router';
@@ -13,6 +14,7 @@ export function AppProviders() {
     <QueryClientProvider client={queryClient}>
       <MantineProvider theme={mantineTheme}>
         <ModalsProvider>
+          <Notifications />
           <RouterProvider router={router} />
           <ReactQueryDevtools initialIsOpen={false} />
         </ModalsProvider>
