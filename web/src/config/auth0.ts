b8b98ed82fa296ec223bfdb3182e66fc01fb50ea
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-03 15:55:06 +0100

    feat: add auth0 specific callback and logout urls

--- web/src/config/auth0.ts
+++ web/src/config/auth0.ts
@@ -2,5 +2,6 @@ export const auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
   audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-  redirectUri: window.location.origin,
+  redirectUri: `${window.location.origin}/auth/login/auth0/callback`,
+  logoutUri: `${window.location.origin}/auth/logout/auth0/callback`,
 };
