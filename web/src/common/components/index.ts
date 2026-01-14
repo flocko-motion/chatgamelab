fc73773d145d885e45790a14877dc72ec81c5fdc
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-15 00:18:56 +0100

    refactor: export semantic components from index

--- web/src/common/components/index.ts
+++ web/src/common/components/index.ts
@@ -1,7 +1,11 @@
-export * from './Button';
+export * from './buttons';
+export * from './typography';
 export * from './Dropdown';
 export * from './DropdownMenu';
 export * from './ErrorBoundary';
 export * from './LanguageSwitcher';
 export * from './TranslationExample';
 export * from './Layout';
+
+// Legacy - will be removed once all usages are migrated to semantic components
+export * from './Button';
