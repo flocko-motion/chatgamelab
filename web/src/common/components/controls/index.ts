0700ac73357c1f50b321b54296d1bb89b811676c
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-17 05:36:09 +0100

    feat: add FilterSegmentedControl component

--- web/src/common/components/controls/index.ts
+++ web/src/common/components/controls/index.ts
@@ -5,6 +5,8 @@
  * 
  * Available components:
  * - SortSelector: Configurable sort dropdown for lists
+ * - FilterSegmentedControl: Configurable segmented control for filters
  */
 
 export { SortSelector, type SortSelectorProps, type SortOption } from './SortSelector';
+export { FilterSegmentedControl, type FilterSegmentedControlProps, type FilterOption } from './FilterSegmentedControl';
