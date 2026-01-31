bea1388e1941fe8cf9896cccd5c1e0d9eaf49141
János Brodbeck <janos.brodbeck@posteo.de>
2026-01-31 16:05:59 +0100

    feat: restructure my-workshop routes and add settings

--- web/src/routes/my-workshop/index.tsx
+++ web/src/routes/my-workshop/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { MyWorkshop } from '@/features/my-workshop';
 
-export const Route = createFileRoute('/my-workshop')({
+export const Route = createFileRoute('/my-workshop/')({
   component: MyWorkshopPage,
 });
 
