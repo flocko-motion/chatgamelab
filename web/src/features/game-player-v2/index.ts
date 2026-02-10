b74d04601e55e5e93188c98a6efde3294b992de1
János Brodbeck <janos.brodbeck@posteo.de>
2026-02-10 20:28:06 +0100

    feat: add guest game player components and hooks

--- web/src/features/game-player-v2/index.ts
+++ web/src/features/game-player-v2/index.ts
@@ -1,4 +1,9 @@
-export { GamePlayer } from './components';
-export * from './types';
-export * from './hooks';
-export * from './context';
+export {
+  GamePlayer,
+  GuestGamePlayer,
+  GuestWelcome,
+  type GuestStartMode,
+} from "./components";
+export * from "./types";
+export * from "./hooks";
+export * from "./context";
