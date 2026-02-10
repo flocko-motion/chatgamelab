381799801995102720aa6f21cf98efe1bfa3c0c3
János Brodbeck <janos.brodbeck@posteo.de>
2026-02-10 23:26:55 +0100

    refactor: update game feature index files to export new shared utilities

--- web/src/features/games/hooks/index.ts
+++ web/src/features/games/hooks/index.ts
@@ -1,2 +1,6 @@
 export { useFavoriteState } from "./useFavoriteState";
 export { useGameNavigation } from "./useGameNavigation";
+export {
+  useGameSessionState,
+  type GameSessionState,
+} from "./useGameSessionState";
